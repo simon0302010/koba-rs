e6d5ad2fc4ca69a2cac8ca9d0d4cad432b4f00dd
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-11-02 21:11:23 +0100

    use parser for args and parse char_range

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,32 @@
-use std::env;
+use std::ops::RangeInclusive;
 use std::path::Path;
 use std::time::Instant;
 
+use clap::Parser;
 use image::{self, GenericImageView};
 use log::*;
 
 fn main() {
+    let args = Args::parse();
+
     match simple_logger::init() {
         Ok(_) => debug!("Logger initialized."),
         Err(e) => println!("WARNING: Failed to initialize logger: {}", e),
     }
 
-    let executable = env::args().next().expect("No executable path found");
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    let img_path = match args.first() {
-        Some(path) => {
-            let file_path = Path::new(path);
-            if !file_path.exists() {
-                error!("File does not exist.");
-                std::process::exit(1);
-            }
-            file_path
-        }
-        None => {
-            error!("Usage: {} IMAGE_PATH", executable);
+    let char_range = match parse_char_range(args.char_range) {
+        Ok(char_range) => char_range,
+        Err(e) => {
+            error!("Failed to parse char range: {}", e);
             std::process::exit(1);
         }
     };
+
+    let img_path = Path::new(&args.image_path);
+    if !img_path.exists() {
+        error!("File does not exist.");
+        std::process::exit(1);
+    }
 
     let image_loading_start = Instant::now();
     let img = match image::open(img_path) {
@@ -43,4 +42,51 @@ fn main() {
     );
 
     info!("Image dimensions: {:?}", img.dimensions());
+    info!("Char Range: {}-{}", *char_range.start(), *char_range.end())
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// image path
+    #[arg(required = true)]
+    image_path: String,
+    /// Unicode character range to use for ASCII art, specified as "start-end" (e.g., "32-126").
+    /// Both start and end should be integer Unicode code points.
+    /// This determines which characters are used to represent image brightness.
+    #[arg(short, long)]
+    char_range: String,
+}
+
+fn parse_char_range(char_range: String) -> Result<RangeInclusive<i32>, String> {
+    if char_range.matches("-").count() != 1 {
+        return Err(
+            "Invalid char_range format. Please use \"start-end\" (e.g., \"32-126\").".to_string(),
+        );
+    }
+
+    let split_range: Vec<&str> = char_range.split("-").map(|s| s.trim()).collect();
+
+    // probably very inefficient in terms of code length
+    let range_start: i32 = match split_range.first() {
+        Some(s) => match s.parse::<i32>() {
+            Ok(start) => start,
+            Err(_) => return Err("Invalid start of range.".to_string()),
+        },
+        None => {
+            return Err("No start value in range.".to_string());
+        }
+    };
+
+    let range_end: i32 = match split_range.get(1) {
+        Some(s) => match s.parse::<i32>() {
+            Ok(end) => end,
+            Err(_) => return Err("Invalid end of range.".to_string()),
+        },
+        None => {
+            return Err("No end value in range.".to_string());
+        }
+    };
+
+    Ok(range_start..=range_end)
 }
