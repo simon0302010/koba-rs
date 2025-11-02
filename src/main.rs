0f53d6abc04fcee39ac1eeafdd2bc7ac1176ec47
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-11-02 18:50:25 +0100

    Simple image loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::path::Path;
+
+use image::{self, GenericImageView};
+
 fn main() {
-    println!("Hello, world!");
+    let executable = env::args().next().expect("No executable path found");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let img_path = match args.first() {
+        Some(path) => {
+            let file_path = Path::new(path);
+            if !file_path.exists() {
+                println!("File does not exist.");
+                std::process::exit(1);
+            }
+            file_path
+        }
+        None => {
+            println!("Usage: {} IMAGE_PATH", executable);
+            std::process::exit(1);
+        }
+    };
+
+    let img = match image::open(img_path) {
+        Ok(img) => img,
+        Err(e) => {
+            println!("Couldn't load image: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Image dimensions: {:?}", img.dimensions());
 }
